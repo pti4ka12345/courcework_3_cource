2f7d4d7d119d64d70baa09db7e30068a44fffb22
pti4ka12345 <96285615+pti4ka12345@users.noreply.github.com>
2022-05-30 22:26:38 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from coursework_3_source.project.dao.models import Director, Movie, User
+from project.dao.models import Director, Movie, User
 from project.config import DevelopmentConfig
 from project.dao.models import Genre
 from project.server import create_app, db
